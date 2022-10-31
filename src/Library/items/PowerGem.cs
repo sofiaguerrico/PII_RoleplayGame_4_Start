85b33dddf0e074899431318555491dedc0d01638
sofiaguerrico <sofia.guerrico@correo.ucu.edu.uy>
2022-10-31 09:27:45 -0300

    Gemas de la realidad y poder

---
+++ src/Library/items/PowerGem.cs
@@ -0,0 +1,43 @@
+namespace RoleplayGame.Items
+{
+    /// <summary>
+    /// Gema del poder. Permite atacar y defensa.
+    /// </summary>
+    public class PowerGem : IAttackItem, IDefenseItem
+    {
+        /// <summary>
+        /// El poder de ataque
+        /// </summary>
+        /// <value></value>
+        public int AttackPower
+        {
+            get
+            {
+                return 16;
+            }
+        }
+
+        /// <summary>
+        /// El poder de defensa.
+        /// </summary>
+        /// <value></value>
+        public int DefensePower
+        {
+            get
+            {
+                return 26;
+            }
+        }
+
+        /// <summary>
+        /// Impresión
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "Gema del poder";
+        }
+
+        
+    }
+}
